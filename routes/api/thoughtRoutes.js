ef9e16e696ef02975b0f37c70279d9c1a56462f3
Williamatthewood <w.wood@tcu.edu>
2023-06-27 17:02:49 -0500

    added thoughtRoutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction,
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+
+//GET all thoughts
+router.route('/').get(getThoughts);
+
+//POST create new thought
+router.route('/').post(createThought);
+
+// /api/thoughts/:thoughtId
+
+//GET single thought
+router.route('/:thoughtId').get(getSingleThought);
+
+//PUT update single thought - update provided in req.body
+router.route('/:thoughtId').put(updateThought);
+
+//DELETE thought
+router.route('/:thoughtId').delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+
+//POST a new reaction - reaction provided in req.body
+router.route('/:thoughtId/reactions').post(addReaction);
+
+//DELETE reaction
+router.route('/:thoughtId/reactions').delete(deleteReaction);
